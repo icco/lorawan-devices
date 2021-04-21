e97b27c7f3624c2c69b4b606814ce8532407903c
Michael Compton <michael.compton@thethingsindustries.com>
2021-04-21 10:06:06 +0200

    Fix go formatting of validate-image/main.go

--- tools/validate-image/main.go
+++ tools/validate-image/main.go
@@ -1,66 +1,80 @@
+// Copyright © 2021 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-  "bytes"
-  "image/png"
-  "image/jpeg"
-  "io/ioutil"
-  "os"
-  "path/filepath"
-  "strings"
+	"bytes"
+	"image/jpeg"
+	"image/png"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func testPNG(path string) bool {
-  // Read image
-  finput, err := os.Open(path)
-  if err != nil {
-    panic(err)
-  }
-  input, err := ioutil.ReadAll(finput)
-  if err != nil {
-    panic(err)
-  }
-  in := bytes.NewReader(input)
-  img, err := png.Decode(in)
-  finput.Close()
-  if err != nil {
-    return false
-  }
+	// Read image
+	finput, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	input, err := ioutil.ReadAll(finput)
+	if err != nil {
+		panic(err)
+	}
+	in := bytes.NewReader(input)
+	img, err := png.Decode(in)
+	finput.Close()
+	if err != nil {
+		return false
+	}
 
 	if img != nil {
 		return true
 	}
 
-  return true
+	return true
 }
 
 func testJPEG(path string) bool {
-  // Read image
-  finput, err := os.Open(path)
-  if err != nil {
-    panic(err)
-  }
-  input, err := ioutil.ReadAll(finput)
-  if err != nil {
-    panic(err)
-  }
-  in := bytes.NewReader(input)
-  img, err := jpeg.Decode(in)
-  finput.Close()
-  if err != nil {
-    return false
-  }
+	// Read image
+	finput, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	input, err := ioutil.ReadAll(finput)
+	if err != nil {
+		panic(err)
+	}
+	in := bytes.NewReader(input)
+	img, err := jpeg.Decode(in)
+	finput.Close()
+	if err != nil {
+		return false
+	}
 
 	if img != nil {
 		return true
 	}
 
-  return true
+	return true
 }
 
 func main() {
-  args := os.Args[1:]
+	args := os.Args[1:]
 	file := args[0]
 
 	if _, err := os.Stat(file); os.IsNotExist(err) {
